5f723d7693f0ba45568b82d4b2df2d948ab93626
gogitIT24 <167851089+gogitIT24@users.noreply.github.com>
2024-05-20 12:23:01 -0400

    lintingv11

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,29 +4,27 @@ from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
     path('register/', views.registration, name='register'),
-    path(route='login', view=views.login_user, name='login'),
-    path(route='logout', view=views.logout_request, name='logout'),
-
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('get_dealers/', views.get_dealerships, name='get_dealers'),
     path(
-        route='get_dealers/<str:state>', 
-        view=views.get_dealerships, 
+        'get_dealers/<str:state>/', 
+        views.get_dealerships, 
         name='get_dealers_by_state'
     ),
-
     path(
-        route='dealer/<int:dealer_id>', 
-        view=views.get_dealer_details,
+        'dealer/<int:dealer_id>/', 
+        views.get_dealer_details, 
         name='dealer_details'
     ),
-
     path(
-        route='reviews/dealer/<int:dealer_id>', 
-        view=views.get_dealer_reviews,
+        'reviews/dealer/<int:dealer_id>/', 
+        views.get_dealer_reviews, 
         name='dealer_details'
     ),
-    path(route='add_review', view=views.add_review, name='add_review'),
-    path(route='get_cars', view=views.get_cars, name='getcars'),
+    path('add_review/', views.add_review, name='add_review'),
+    path('get_cars/', views.get_cars, name='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
