18ba9df30c9bde8124c93f73620676630f750e3c
gogitIT24 <167851089+gogitIT24@users.noreply.github.com>
2024-05-20 02:49:04 -0400

    application finished stage

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,3 +1,4 @@
+import PostReview from "./components/Dealers/PostReview"
 import Dealer from "./components/Dealers/Dealer"
 import Dealers from './components/Dealers/Dealers';
 import React from 'react';
@@ -12,6 +13,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers/>} />
       <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
