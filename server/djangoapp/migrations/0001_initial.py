ca400975e9fb10cd6ea852e9b3909cb418868f8e
gogitIT24 <167851089+gogitIT24@users.noreply.github.com>
2024-05-20 12:36:25 -0400

    changes

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -9,16 +9,16 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = []  # No indentation needed for empty lists
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
             name='CarMake',
             fields=[
                 ('id', models.BigAutoField(
-                    auto_created=True, 
-                    primary_key=True, 
-                    serialize=False, 
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
                     verbose_name='ID'
                 )),
                 ('name', models.CharField(max_length=100)),
@@ -29,31 +29,31 @@ class Migration(migrations.Migration):
             name='CarModel',
             fields=[
                 ('id', models.BigAutoField(
-                    auto_created=True, 
-                    primary_key=True, 
-                    serialize=False, 
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
                     verbose_name='ID'
                 )),
                 ('dealer_id', models.IntegerField()),
                 ('name', models.CharField(max_length=100)),
                 ('type', models.CharField(
                     choices=[
-                        ('SEDAN', 'Sedan'), 
-                        ('SUV', 'SUV'), 
+                        ('SEDAN', 'Sedan'),
+                        ('SUV', 'SUV'),
                         ('WAGON', 'Wagon')
-                    ], 
-                    default='SUV', 
+                    ],
+                    default='SUV',
                     max_length=10
                 )),
                 ('year', models.IntegerField(
-                    default=2023, 
+                    default=2023,
                     validators=[
                         django.core.validators.MaxValueValidator(2023),
                         django.core.validators.MinValueValidator(2015)
                     ]
                 )),
                 ('car_make', models.ForeignKey(
-                    on_delete=django.db.models.deletion.CASCADE, 
+                    on_delete=django.db.models.deletion.CASCADE,
                     to='djangoapp.carmake'
                 )),
             ],
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,18 +11,18 @@ urlpatterns = [
     path('logout/', views.logout_request, name='logout'),
     path('get_dealers/', views.get_dealerships, name='get_dealers'),
     path(
-        'get_dealers/<str:state>/', 
-        views.get_dealerships, 
+        'get_dealers/<str:state>/',
+        views.get_dealerships,
         name='get_dealers_by_state'
     ),
     path(
-        'dealer/<int:dealer_id>/', 
-        views.get_dealer_details, 
+        'dealer/<int:dealer_id>/',
+        views.get_dealer_details,
         name='dealer_details'
     ),
     path(
-        'reviews/dealer/<int:dealer_id>/', 
-        views.get_dealer_reviews, 
+        'reviews/dealer/<int:dealer_id>/',
+        views.get_dealer_reviews,
         name='dealer_details'
     ),
     path('add_review/', views.add_review, name='add_review'),
